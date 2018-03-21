6846d239f3066ad9a897f12db2bb82c98e7f6ef8
e-crow-7 <e-crow-7@users.noreply.github.com>
2018-03-21 18:04:49 +0100

    SCSS bundling

--- _web-orthoja/config/webpack.config.dev.js
+++ _web-orthoja/config/webpack.config.dev.js
@@ -188,6 +188,31 @@ module.exports = {
               },
             ],
           },
+          // "SCSS" loader allows the use of scss file for easier and cleaner styling.
+          {
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: require.resolve('style-loader'),
+                },
+                {
+                    loader: require.resolve('css-loader'),
+                    options: {
+                        modules: true,
+                        localIdentName: '[name]_[local]_[hash:base64:8]',
+                        importLoaders: 1,
+                        sourceMap: true,
+                        minimize: true,
+                    },
+                 },
+                 {
+                     loader: require.resolve("sass-loader"),
+                     options: {
+                        sourceMap: true,
+                     }
+                 },
+            ]
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
